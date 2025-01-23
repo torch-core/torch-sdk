f7a34103a662011c963a8fd0e2bded71e2c01e42
ipromise2324 <168allen55@gmail.com>
2025-01-23 18:34:03 +0800

    feat: add balance checking utilities for Jetton and TON in tests
    - Introduced new utility functions in check.ts to validate Jetton and TON balance changes during swap operations.
    - Implemented checks for balance increase, decrease, and no change for Jetton wallets.
    - Added a function to verify TON balance decrease only by gas fees.
    - Updated swap.spec.ts to utilize these new utilities, enhancing the accuracy and reliability of swap tests.
    - Improved test setup by initializing blockchain state before each test to ensure consistent testing conditions.

--- test/swap.spec.ts
+++ test/swap.spec.ts
@@ -1,10 +1,11 @@
-import { SandboxContract } from '@ton/sandbox';
+import { Blockchain, BlockchainSnapshot, SandboxContract } from '@ton/sandbox';
 import { TorchSDK } from '../src';
 import { initialize } from './setup';
 import { PoolAssets } from './config';
 import { Address, SenderArguments, toNano } from '@ton/core';
 import { SwapParams } from '../src/types/swap';
 import { JettonWallet } from '@ton/ton';
+import { checkJettonBalDecrease, checkJettonBalIncrease, checkJettonBalNotChanged, checkTONBalDecrease } from './check';
 
 describe('Swap Testcases', () => {
   // set timeout: 6 minutes
@@ -12,6 +13,8 @@ describe('Swap Testcases', () => {
 
   let torchSDK: TorchSDK;
   let sender: Address;
+  let blockchain: Blockchain;
+  let initBlockchainState: BlockchainSnapshot;
 
   // Sender Jetton Wallet
   let senderStTONWallet: SandboxContract<JettonWallet>;
@@ -19,6 +22,7 @@ describe('Swap Testcases', () => {
   //   let senderHTONWallet: SandboxContract<JettonWallet>;
 
   // Sender Asset Balance Before
+  let senderTonBalBefore: bigint;
   let senderStTONBalBefore: bigint;
   let senderTsTONBalBefore: bigint;
   //   let senderHTONBalBefore: bigint;
@@ -28,7 +32,7 @@ describe('Swap Testcases', () => {
   beforeAll(async () => {
     ({
       torchSDK,
-      //   blockchain,
+      blockchain,
       //   factory,
       sender,
       //   triTONPool,
@@ -37,34 +41,112 @@ describe('Swap Testcases', () => {
       //   senderHTONWallet,
       send,
     } = await initialize());
+
+    initBlockchainState = blockchain.snapshot();
   });
 
   beforeEach(async () => {
+    // Reset blockchain state
+    await blockchain.loadFrom(initBlockchainState);
+
+    // Get sender balance
+    senderTonBalBefore = await (await blockchain.getContract(sender)).balance;
     senderStTONBalBefore = await senderStTONWallet.getBalance();
     senderTsTONBalBefore = await senderTsTONWallet.getBalance();
     // senderHTONBalBefore = await senderHTONWallet.getBalance();
   });
 
-  it('should swap tsTON to stTON', async () => {
-    // Build swap paylaod
-    const amountIn = toNano('0.05');
-    const swapParams: SwapParams = {
+  async function swapImpact() {
+    const swapFluctuateParams: SwapParams = {
       mode: 'ExactIn',
       assetIn: PoolAssets.tsTONAsset,
       assetOut: PoolAssets.stTONAsset,
-      amountIn,
+      amountIn: toNano('1'),
     };
+    const sendFluctuateArgs = await torchSDK.getSwapPayload(sender, swapFluctuateParams);
+    await send(sendFluctuateArgs);
+  }
 
-    // Send swap
-    const sendArgs = await torchSDK.getSwapPayload(sender, swapParams);
-    await send(sendArgs);
+  describe('TriTON Pool', () => {
+    it('should swap tsTON to stTON', async () => {
+      // Build swap paylaod
+      const amountIn = toNano('0.05');
+      const swapParams: SwapParams = {
+        mode: 'ExactIn',
+        assetIn: PoolAssets.tsTONAsset,
+        assetOut: PoolAssets.stTONAsset,
+        amountIn,
+      };
 
-    // Sender stTON balance should be increased
-    const senderStTONBalAfter = await senderStTONWallet.getBalance();
-    expect(senderStTONBalAfter).toBeGreaterThan(senderStTONBalBefore);
+      // Send swap
+      const sendArgs = await torchSDK.getSwapPayload(sender, swapParams);
+      await send(sendArgs);
 
-    // Sender tsTON balance should be decreased
-    const senderTsTONBalAfter = await senderTsTONWallet.getBalance();
-    expect(senderTsTONBalAfter).toBeLessThan(senderTsTONBalBefore);
+      // Sender stTON balance should be increased
+      await checkJettonBalIncrease(senderStTONWallet, senderStTONBalBefore);
+
+      // Sender tsTON balance should be decreased
+      await checkJettonBalDecrease(senderTsTONWallet, senderTsTONBalBefore, amountIn);
+    });
+
+    it('should refund tsTON to sender when min amount out is not met (tsTON -> stTON)', async () => {
+      // Build swap paylaod with min amount out
+      const amountIn = toNano('0.05');
+      const swapParams: SwapParams = {
+        mode: 'ExactIn',
+        assetIn: PoolAssets.tsTONAsset,
+        assetOut: PoolAssets.stTONAsset,
+        amountIn,
+        minAmountOut: toNano('1'),
+      };
+
+      // Send swap
+      const sendArgs = await torchSDK.getSwapPayload(sender, swapParams);
+      await send(sendArgs);
+
+      // Sender stTON balance should be not changed
+      await checkJettonBalNotChanged(senderStTONWallet, senderStTONBalBefore);
+
+      // Build swap payload with slippage
+      const swapSlipageParams: SwapParams = {
+        mode: 'ExactIn',
+        assetIn: PoolAssets.tsTONAsset,
+        assetOut: PoolAssets.stTONAsset,
+        amountIn,
+        slippageTolerance: 0.01,
+      };
+
+      // Someone swap to make the price fluctuate
+      await swapImpact();
+
+      // Send swap
+      senderStTONBalBefore = await senderStTONWallet.getBalance();
+      const sendSlipageArgs = await torchSDK.getSwapPayload(sender, swapSlipageParams);
+      await send(sendSlipageArgs);
+
+      // Sender stTON balance should be not changed
+      await checkJettonBalNotChanged(senderStTONWallet, senderStTONBalBefore);
+    });
+
+    it('should swap TON to stTON', async () => {
+      // Build swap paylaod
+      const amountIn = toNano('0.05');
+      const swapParams: SwapParams = {
+        mode: 'ExactIn',
+        assetIn: PoolAssets.tonAsset,
+        assetOut: PoolAssets.stTONAsset,
+        amountIn,
+      };
+
+      // Send swap
+      const sendArgs = await torchSDK.getSwapPayload(sender, swapParams);
+      await send(sendArgs);
+
+      // Sender stTON balance should be increased
+      await checkJettonBalIncrease(senderStTONWallet, senderStTONBalBefore);
+
+      // Sender TON balance should be decreased
+      await checkTONBalDecrease(blockchain, sender, senderTonBalBefore);
+    });
   });
 });
