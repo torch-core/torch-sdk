c025530e3c4230becfd4ec9783a4c16758856805
alan890104 <alan890104@gmail.com>
2025-01-25 02:18:45 +0800

    refactor: reorganize exports in index.ts for improved clarity
    - Rearranged export statements in index.ts to group related functionalities, enhancing readability.
    - Moved SDK-related exports above API exports for better logical structure.
    - Ensured that utility functions and simulator types are now correctly positioned, improving overall code organization.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-// Utils
-export { generateQueryId, toUnit } from './utils';
-
-// Simulator
-export type { SimulateDepositResponse, SimulateWithdrawResponse, SimulateSwapResponse } from './types/simulator';
+// SDK
+export type { SwapParams, DepositParams, WithdrawParams } from './types/sdk';
+export { TorchSDK, type TorchSDKOptions } from './core/sdk';
 
 // API
 export { TorchAPI, type TorchAPIOptions } from './core/api';
 
-// SDK
-export type { SwapParams, DepositParams, WithdrawParams } from './types/sdk';
-export { TorchSDK, type TorchSDKOptions } from './core/sdk';
+// Simulator
+export type { SimulateDepositResponse, SimulateWithdrawResponse, SimulateSwapResponse } from './types/simulator';
+
+// Utils
+export { generateQueryId, toUnit } from './utils';
